ac2746bdc3f6a168c25910b2a1358ff3c01d1526
xiewenjun <xiewenjun@chelun.com>
2017-07-22 01:08:17 +0800

    push本地仓库到远程仓库

---
+++ app/src/main/java/com/jwj/self/smartlockdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jwj.self.smartlockdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //添加的新东西
+    }
+}
